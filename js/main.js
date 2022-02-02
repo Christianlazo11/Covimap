a6ceb5c0626bbd24cf1e28db9133730ad937ed7e
christian lazo <kenishi.cristianxd@gmail.com>
2022-02-02 16:52:33 -0500

    seccion 1 y dos

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 20,
+  effect: "fade",
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
