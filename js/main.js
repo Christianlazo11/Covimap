d1c5175d29dae506157ff098890fb0ad2dc1e5fd
christian lazo <kenishi.cristianxd@gmail.com>
2022-02-03 09:40:19 -0500

    completados xD

--- js/main.js
+++ js/main.js
@@ -10,3 +10,35 @@ function showText(p) {
   const data = document.getElementById(p.dataset.panel);
   data.classList.toggle("active");
 }
+
+const nav = document.getElementById("mynav");
+const navItems = document.querySelectorAll(".nav-item-text");
+const logo = document.querySelector(".rotate");
+
+window.onscroll = function () {
+  console.log(window.innerWidth);
+  if (window.innerWidth > 0) {
+    if (window.scrollY > 300) {
+      nav.classList.remove("bg-transparent");
+      nav.classList.add("bg-white");
+      navItems.forEach((item) => {
+        item.classList.remove("text-white");
+        item.classList.add("text-blue-primary");
+      });
+      logo.src = "./images/coronavirus.png";
+      console.log(logo);
+    } else {
+      nav.classList.remove("bg-white");
+      nav.classList.add("bg-transparent");
+      navItems.forEach((item) => {
+        item.classList.remove("text-blue-primary");
+        item.classList.add("text-white");
+      });
+      logo.src = "./images/coronavirus_white.png";
+    }
+  } else if (window.innerWidth <= 990) {
+    // nav.classList.remove("bg-white");
+    // nav.classList.remove("bg-transparent");
+    // nav.classList.add("bg-dark");
+  }
+};
