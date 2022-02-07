6324c687c1d80717588d1637ab2b1165ab1bf86a
christian lazo <kenishi.cristianxd@gmail.com>
2022-02-07 12:09:19 -0500

    PaginaPrincipalFuncionalidad

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+//Funcion para Realizar la peticion a la api
+getData();
+
+setInterval(() => {
+  getData();
+}, 8000);
+
+//Colocamos las varibales para cargar un preloader al iniciar la pagina
 const loader = document.querySelector("#loader");
 
 window.addEventListener("load", () => {
@@ -26,7 +34,6 @@ const navItems = document.querySelectorAll(".nav-item-text");
 const logo = document.querySelector(".rotate");
 
 window.onscroll = function () {
-  console.log(window.innerWidth);
   if (window.innerWidth > 0) {
     if (window.scrollY > 300) {
       nav.classList.remove("bg-transparent");
@@ -36,7 +43,6 @@ window.onscroll = function () {
         item.classList.add("text-blue-primary");
       });
       logo.src = "./images/coronavirus.png";
-      console.log(logo);
     } else {
       nav.classList.remove("bg-white");
       nav.classList.add("bg-transparent");
@@ -51,4 +57,49 @@ window.onscroll = function () {
     // nav.classList.remove("bg-transparent");
     // nav.classList.add("bg-dark");
   }
+};
+
+// Funcion que realiza la peticion a la apo
+function getData() {
+  //Creamos una url de donde vamos a obtener los paises
+  const url = "https://disease.sh/v3/covid-19/countries";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      getRandomData(data);
+    });
+}
+
+// Funcion que obtiene un pais random de la informacion que devolvio la api
+function getRandomData(data) {
+  let countryRandom = Math.floor(Math.random() * (data.length - 1 + 1) + 1);
+
+  let dataCountryRandom = data[countryRandom];
+  printCardCountry(dataCountryRandom);
+}
+//Funcion que pinta la informacion en el html
+function printCardCountry(data) {
+  const bodyCard = document.getElementById("card-banner");
+  let body = `<div class="card-banner-content d-flex flex-row justify-content-between">
+  <div class="d-flex justify-content-center align-items-center ">
+    <img src="${data.countryInfo.flag}" alt="flag">
+  </div>
+
+  <div>
+    <h1 class="text-danger fs-3">${data.cases}</h1>
+    <div class="d-flex justify-content-around align-items-center">
+      <i class="fas fa-chart-line text-danger fs-5"></i>
+      <span class="botton-orange">${data.active}</span>
+      <span class="botton-green">${data.recovered}</span>
+    </div>
+  </div>
+  </div>`;
+
+  bodyCard.innerHTML = body;
+}
+
+//Evento para que al darle click a la targeta esta cambie de pais
+const bodyCardEvent = document.getElementById("card-banner");
+bodyCardEvent.onclick = () => {
+  getData();
 };
